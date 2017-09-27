b85226518177604187ac5927266810f89074bcc1
John Ritsema <john.ritsema@turner.com>
2017-09-27 13:49:42 -0400

    prep for ci/cd

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,19 @@
-FROM golang:1.8.3-onbuild
+FROM golang:1.8.3 AS build
+WORKDIR /go/src/github.com/turnerlabs/turner-defaultbackend
+COPY Gopkg.toml .
+COPY Gopkg.lock .
+COPY main.go .
+
+# install deps
+RUN go get -v github.com/golang/dep/cmd/dep
+RUN dep ensure -v
+
+# compile server
+RUN GOOS=linux GOARCH=386 go build -v -o app .
+
+### run
+FROM alpine:3.6
+WORKDIR /root/
+COPY --from=build /go/src/github.com/turnerlabs/turner-defaultbackend/app .
+CMD ["./app"]
+###
--- main.go
+++ main.go
@@ -1,50 +1,51 @@
 package main
 
 import (
-  "os"
-  "fmt"
-  "net/http"
-  "github.com/gorilla/handlers"
-  "github.com/gorilla/mux"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 )
 
 func main() {
 
-  var port string
+	var port string
 
-  port = getenv("PORT", "8001")
-  fmt.Println("Listening on : " + port)
+	port = getenv("PORT", "8001")
+	fmt.Println("Listening on : " + port)
 
-  r := mux.NewRouter()
+	r := mux.NewRouter()
 
-  r.HandleFunc("/healthz", HealthCheck)
-  r.HandleFunc("/{blah:.*}", Default)
-  r.HandleFunc("/", Default)
+	r.HandleFunc("/healthz", healthCheck)
+	r.HandleFunc("/{blah:.*}", defaultRoute)
+	r.HandleFunc("/", defaultRoute)
 
-  loggedRouter := handlers.LoggingHandler(os.Stdout, r)
+	loggedRouter := handlers.LoggingHandler(os.Stdout, r)
 
-  http.Handle("/", r)
+	http.Handle("/", r)
 
-  if os.Getenv("ENABLE_LOGGING") == "true" {
-    http.ListenAndServe(":" + port, loggedRouter)
-  } else {
-    http.ListenAndServe(":" + port, nil)
-  }
+	if os.Getenv("ENABLE_LOGGING") == "true" {
+		http.ListenAndServe(":"+port, loggedRouter)
+	} else {
+		http.ListenAndServe(":"+port, nil)
+	}
 }
 
-func HealthCheck(w http.ResponseWriter, r *http.Request) {
-  var x string = "{\"Hello\":" + "\"" + os.Getenv("PRODUCT") + "-" + os.Getenv("ENVIRONMENT") + "\"}"
-  w.Write([]byte(x))
+func healthCheck(w http.ResponseWriter, r *http.Request) {
+	var x = "{\"Hello\":" + "\"" + os.Getenv("PRODUCT") + "-" + os.Getenv("ENVIRONMENT") + "\"}"
+	w.Write([]byte(x))
 }
 
-func Default(w http.ResponseWriter, r *http.Request) {
-  http.Error(w, "Default Backend", http.StatusNotImplemented)
+func defaultRoute(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "Default Backend", http.StatusNotImplemented)
 }
 
 func getenv(key, fallback string) string {
-    value := os.Getenv(key)
-    if len(value) == 0 {
-      return fallback
-    }
-    return value
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return fallback
+	}
+	return value
 }
